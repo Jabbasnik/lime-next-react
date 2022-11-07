b26d0ca6e60260c729abf5f2be3c021ead3161d7
Marcin Jackowiak <marcin.jackowiak94@gmail.com>
2022-11-07 09:16:04 +0100

    Added localhost addresses

--- constants/index.ts
+++ constants/index.ts
@@ -13,4 +13,5 @@
   export const supportedMetamaskNetworks = [1, 3, 4, 5, 42, 31337];
 
   export const ALBT_TOKEN_ADDRESS = "0xc6869a93ef55e1d8ec8fdcda89c9d93616cf0a72";
-  export const US_ELECTION_ADDRESS = "0xcc4149bC41a92FA4E104e7555ff50989de18d50A";
+  export const US_ELECTION_ADDRESS = "0xcc4149bC41a92FA4E104e7555ff50989de18d50A"; // goerli
+  // export const US_ELECTION_ADDRESS = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"; // localhost
